9efc9cb94332d29707e1da58acb62ce2477ed5e5
kpgpulgares <kpgpulgares@chinabank.ph>
2025-04-25 21:57:42 +0800

    use mutation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import DisplayData from './DisplayData';
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from "@apollo/client"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
 function App() {
   const client = new ApolloClient({
--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,4 +1,4 @@
-import { useQuery, useLazyQuery, gql } from '@apollo/client'
+import { useQuery, useLazyQuery, gql, useMutation } from '@apollo/client'
 import { useState } from 'react'
 
 const QUERY_ALL_USERS = gql`
@@ -28,30 +28,55 @@ const QUERY_MOVIE_BY_NAME = gql`
     }
 `
 
+const CREATE_USER = gql`
+    mutation CreateUser($input: CreateUser!) {
+        createUser(input: $input) {
+            id
+            name
+            age
+            username
+        }
+    }
+`
+
 const DisplayData = () => {
     const [movieSearch, setMovieSearch] = useState("")
-    const { data, loading, error } = useQuery(QUERY_ALL_USERS)
+    const [username, setUsername] = useState("")
+    const [name, setName] = useState("")
+    const [age, setAge] = useState(0)
+    const [nationality, setNationality] = useState("")
+    const { data, loading, refetch } = useQuery(QUERY_ALL_USERS)
     const { data: movieData } = useQuery(QUERY_ALL_MOVIES)
     const [getMovieByName, { data: movieDataByName, error: movieError }] = useLazyQuery(QUERY_MOVIE_BY_NAME)
+    const [createUser] = useMutation(CREATE_USER)
 
     if (loading) {
         return <h1>Loading...</h1>
     }
 
-    if (data) {
-        console.log(data)
-    }
-
-    if (error) {
-        console.log(error)
-    }
-
-    if(movieError) {
-        console.log(movieError)
-    }
-
     return (
         <>
+            <div>
+                <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" placeholder="name" onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder="age" onChange={(e) => setAge(Number(e.target.value))} />
+                <input type="text" placeholder="Nationality" onChange={(e) => setNationality(e.target.value)} />
+                <button onClick={() => {
+                    createUser({
+                        variables: {
+                            input: {
+                                name,
+                                username,
+                                age,
+                                nationality
+                            }
+                        }
+                    })
+                    refetch()
+                }}>
+                    Submit
+                </button>
+            </div>
             <div>
                 {data && data.users.map((user) => {
                     return <div>
